cde449f543a9327e9bb5a5f6fca966c9934242ec
Jakoub <118178881+Jak0ub@users.noreply.github.com>
2024-11-04 17:38:49 +0100

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -6,8 +6,8 @@ if %errorlevel% equ 0 (goto kill) else (cls & echo Process not found & timeout /
 :kill
 cls
 echo Process found!
-timeout /nobreak /t 1 >nul
 set /p oke=Do you want to terminate the process? (1=Yes, other=No): 
 if %oke% equ 1 (taskkill /IM %user% -F & taskkill /IM %user%.exe -F & cls) else (echo Exiting & timeout /nobreak /t 3 >nul & exit)
+timeout /nobreak /t 1 >nul
 tasklist | findstr /i %user% >nul
 if %errorlevel% equ 0 (echo Unable to terminate process & timeout /nobreak /t 5 >nul & exit) else (echo Process terminated successfully & timeout /nobreak /t 5 >nul & exit)
