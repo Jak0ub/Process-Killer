68729581f7b3a8c57a261615572c3a35926a6433
Jakoub <118178881+Jak0ub@users.noreply.github.com>
2024-11-04 17:40:27 +0100

    Update killer.bat

--- killer.bat
+++ killer.bat
@@ -1,13 +1,14 @@
 @echo off
 :repeat
-set /p user=Enter process to search for: 
+set /p user=Enter process for search: 
 tasklist | findstr /i %user% >nul
-if %errorlevel% equ 0 (goto kill) else (cls & echo Process not found & timeout /nobreak /t 1 >nul & goto repeat)
+if %errorlevel% equ 0 (goto kill)else (cls & echo process not found! & timeout /nobreak /t 1 >nul & goto repeat)
 :kill
 cls
-echo Process found!
-set /p oke=Do you want to terminate the process? (1=Yes, other=No): 
-if %oke% equ 1 (taskkill /IM %user% -F & taskkill /IM %user%.exe -F & cls) else (echo Exiting & timeout /nobreak /t 3 >nul & exit)
+echo process found!
+timeout /nobreak /t 1 >nul
+set /p oke=End process?(1=Yes, other=No): 
+if %oke% equ 1 (taskkill /IM %user% -F & taskkill /IM %user%.exe -F & cls)else (echo exiting... & timeout /nobreak /t 3 >nul & exit)
 timeout /nobreak /t 1 >nul
 tasklist | findstr /i %user% >nul
-if %errorlevel% equ 0 (echo Unable to terminate process & timeout /nobreak /t 5 >nul & exit) else (echo Process terminated successfully & timeout /nobreak /t 5 >nul & exit)
+if %errorlevel% equ 0 (echo process wasn't able to be ended & timeout /nobreak /t 5 >nul & exit )else (echo process sucessfully ended & timeout /nobreak /t 5 >nul & exit)
